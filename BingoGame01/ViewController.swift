cad99c3be3a9559919e746a7792057304e69e8c6
井上航 <k.prfm19@gmail.com>
2015-12-16 01:55:30 +0900

    Initial Commit

---
+++ BingoGame01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BingoGame01
+//
+//  Created by 井上航 on 2015/12/16.
+//  Copyright © 2015年 Wataru Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
